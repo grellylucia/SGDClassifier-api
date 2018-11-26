1aaa49c31efa58c364d01ef3520dc0018b751fb8
Unknown <grellylondo@gmail.com>
2018-11-26 17:32:45 +0700

    update

---
+++ SGDdeploy.py
@@ -0,0 +1,50 @@
+from flask import Flask,jsonify,request
+from flasgger import Swagger
+from sklearn.externals import joblib
+import numpy as np
+from flask_cors import CORS
+
+import pandas as pd
+
+app = Flask(__name__)
+Swagger(app)
+CORS(app)
+
+@app.route('/input/task', methods=['POST'])
+def predict():
+    """
+    Ini Adalah Endpoint Untuk Mengklasifikasi Artikel
+    ---
+    tags:
+        - Rest Controller
+    parameters:
+      - name: body
+        in: body
+        required: true
+        schema:
+          id: Artikel
+          required:
+            - textArtikel
+          properties:
+            textArtikel:
+              type: string
+              description: Please input with valid text.
+              default: 0
+    responses:
+        200:
+            description: Success Input
+    """
+    new_task = request.get_json()
+
+    textArtikel = new_task['textArtikel']
+
+    X_New = np.array([textArtikel])
+
+    pipe = joblib.load('SGDClassifier-new2.pkl')
+
+    resultPredict = pipe[0].predict(X_New)
+
+    return jsonify({'message': format(resultPredict)})
+
+if __name__ == '__main__' :
+ app.run(debug=True)
